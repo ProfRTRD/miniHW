5702f80e7a00628412b54b7cf17ae1c35fa8cb84
juan jose <jgonzalezfl@unal.edu.co>
2022-06-09 11:41:13 -0500

    a

--- miniHWtdy.cpp
+++ miniHWtdy.cpp
@@ -10,15 +10,11 @@ int main(void)
 {
   const int N =1000;
   std::valarray<double> v1(N);
-  std::iota(v1.begin(), v1.end(), 0.0);
+  std::iota(begin(v1), end(v1), 0.0);
   const double DX = 2*M_PI/N;
   v1=v1*DX;
   std::valarray<double> v2 = std::sin(v1);
   v2 = v2[std::abs(v2) < 0.5];
-  for(auto n : v2) {
-    if(n!=0){
-      std::cout << n<<"  ";
-    }
-    std::cout << "/n";
-  }
+  for(int n : v2) std::cout << n <<std::endl;
 }
+
